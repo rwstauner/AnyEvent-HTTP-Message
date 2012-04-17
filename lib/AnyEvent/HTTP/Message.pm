f59ab26622cefed2ee8b36251f9087a499258a6e
Randy Stauner <randy@magnificent-tears.com>
2012-04-16 20:24:21 -0700

    Define base constructor and document (slightly)

--- lib/AnyEvent/HTTP/Message.pm
+++ lib/AnyEvent/HTTP/Message.pm
@@ -3,12 +3,30 @@ use strict;
 use warnings;
 
 package AnyEvent::HTTP::Message;
-# ABSTRACT: undef
+# ABSTRACT: Base class for AnyEvent::HTTP Request/Response
+
+sub new {
+  my $class = shift;
+  my $self = @_ == 1 && ref($_[0]) eq 'HASH'
+      # if passed a single hashref take a shallow copy
+    ? { %{ $_[0] } }
+      # otherwise it's the argument list for http_request()
+    : $class->parse_args(@_);
+  bless $self, $class;
+}
 
 1;
 
 =head1 SYNOPSIS
 
+  # don't use this directly
+
 =head1 DESCRIPTION
+
+This is a base class for:
+
+=for :list
+* L<AnyEvent::HTTP::Request>
+* L<AnyEvent::HTTP::Response>
 
 =cut
